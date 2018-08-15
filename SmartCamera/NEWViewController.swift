a8cfb544acc0cc3d8df4f9ab602c5dc5510127a1
Angadjot singh <angadjotsingh@Angadjots-MacBook-Air.local>
2018-08-15 14:31:16 +0530

    new file added

---
+++ SmartCamera/NEWViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NEWViewController.swift
+//  SmartCamera
+//
+//  Created by Angadjot singh on 15/08/18.
+//  Copyright © 2018 Angadjot singh. All rights reserved.
+//
+
+import UIKit
+
+class NEWViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
